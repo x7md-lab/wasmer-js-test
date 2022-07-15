025bc51bc3641f8a84420070b6f94c877b93fbfe
x7md <x7mdNet@gmail.com>
2022-07-15 15:09:02 +0300

    try use jqs

--- src/main.mjs
+++ src/main.mjs
@@ -1,5 +1,4 @@
 import { init, WASI } from '@wasmer/wasi';
-
 await init();
 
 let wasi = new WASI({
@@ -8,11 +7,14 @@ let wasi = new WASI({
       // 'ENVVAR2': '2'
   },
   args: [
-      // 'command', 'arg1', 'arg2'
+      'qjs', '--help'
   ],
+  bindings: {
+    ...WASI.defaultBindings,
+  }
 });
 
-const moduleBytes = fetch("./demo.wasm");
+const moduleBytes = fetch("./qjs.wasm");
 const module = await WebAssembly.compileStreaming(moduleBytes);
 // Instantiate the WASI module
 await wasi.instantiate(module, {});
@@ -22,4 +24,4 @@ let exitCode = wasi.start();
 let stdout = wasi.getStdoutString();
 
  // This should print "hello world (exit code: 0)"
-console.log(`${stdout}(exit code: ${exitCode})`);
+console.log(`${stdout}`);
